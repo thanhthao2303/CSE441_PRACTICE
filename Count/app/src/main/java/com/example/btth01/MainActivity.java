79f14cd0f2f4d5d4468f644b748f71c12d49e92e
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-19 01:54:57 +0700

    android

---
+++ Count/app/src/main/java/com/example/btth01/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.btth01;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB, edtKQ;
+    Button btntong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btntong = findViewById(R.id.btntong);
+        btntong.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+     int a = Integer.parseInt(edtA.getText().toString());
+     int b = Integer.parseInt(edtB.getText().toString());
+
+     int c = a + b;
+     edtKQ.setText(c+""); // Hiển thị kết quả
+ }
+        });
+    }
+}
