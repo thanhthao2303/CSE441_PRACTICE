261c801bf9cc252a9f30962eecf67533f69c290d
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-21 04:29:05 +0700

    Fahrenheit

--- Fahrenheit/app/src/main/java/com/example/fahrenheit/MainActivity.java
+++ Fahrenheit/app/src/main/java/com/example/fahrenheit/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.fahrenheit;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class MainActivity extends Activity {
+EditText txtCel,txtFar;
+Button btnCel, btnFar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+            txtFar = findViewById(R.id.txtFar);
+            txtCel = findViewById(R.id.txtCel);
+            btnFar = findViewById(R.id.btnFar);
+            btnCel = findViewById(R.id.btnCel);
+            btnCel.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onClick(View view) {
+                    DecimalFormat dcf=new DecimalFormat("#.00");
+                    String doC = txtCel.getText()+"";
+                    int C=Integer.parseInt(doC);
+                    txtFar.setText(dcf.format(C * 1.8 + 32));
+                }
+            });
+            btnFar.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onClick(View view) {
+                    DecimalFormat dcf=new DecimalFormat("#.00");
+// TODO Auto-generated method stub
+                    String doF = txtFar.getText()+"";
+                    int F=Integer.parseInt(doF);
+                    txtCel.setText(dcf.format((F - 32) / 1.8));
+                }
+            });
+
+
     }
 }
+
+
+
