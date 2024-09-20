740be60e911678c7527d4805b12884eff491b6fa
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-21 01:51:40 +0700

    Project_cal

--- Project_cal/app/src/main/java/com/example/project_cal/MainActivity.java
+++ Project_cal/app/src/main/java/com/example/project_cal/MainActivity.java
@@ -1,24 +1,189 @@
+//package com.example.project_cal;
+//
+//import android.annotation.SuppressLint;
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//
+//import androidx.activity.EdgeToEdge;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+//
+//public class MainActivity extends Activity {
+//    EditText edt1, edt2, edt3;
+//    Button btncong, btntru, btnnhan, btnchia;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+////        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+////            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+////            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+////            return insets;
+////        });
+//
+//        edt1 = findViewById(R.id.edta);
+//        edt2 = findViewById(R.id.edtb);
+//        edt3 = findViewById(R.id.edtc);
+//        btncong = findViewById(R.id.btncong);
+//        btntru = findViewById(R.id.btntru);
+//        btnchia = findViewById(R.id.btnchia);
+//        btnnhan = findViewById(R.id.btnnhan);
+//        btncong.setOnClickListener(new View.OnClickListener() {
+//            @SuppressLint("SetTextI18n")
+//            @Override
+//            public void onClick(View view) {
+//                int a = Integer.parseInt("0"+edt1.getText());
+//                int b = Integer.parseInt("0"+edt2.getText());
+//// TODO Auto-generated method stub
+//                edt3.setText("a+b=" +(a+b));
+//            }
+//        });
+//
+//          btntru.setOnClickListener(new View.OnClickListener() {
+//              @SuppressLint("SetTextI18n")
+//              @Override
+//              public void onClick(View view) {
+//                  int a = Integer.parseInt("0"+edt1.getText());
+//                  int b = Integer.parseInt("0"+edt2.getText());
+//// TODO Auto-generated method stub
+//                  edt3.setText("a - b =" +(a-b));
+//              }
+//          });
+//
+//          btnnhan.setOnClickListener(new View.OnClickListener() {
+//              @SuppressLint("SetTextI18n")
+//              @Override
+//              public void onClick(View view) {
+//                  int a = Integer.parseInt("0"+edt1.getText());
+//                  int b = Integer.parseInt("0"+edt2.getText());
+//// TODO Auto-generated method stub
+//                  edt3.setText("a * b =" +(a*b));
+//              }
+//          });
+//
+//          btnchia.setOnClickListener(new View.OnClickListener() {
+//              @SuppressLint("SetTextI18n")
+//              @Override
+//              public void onClick(View view) {
+//                  int a = Integer.parseInt("0"+edt1.getText());
+//                  int b = Integer.parseInt("0"+edt2.getText());
+//                  if (b == 0)
+//                  {
+//                      edt3.setText("B phai khac 0");
+//                  }
+//                  else
+//                  {
+//                      edt3.setText("a / b =" +(a/b));
+//                  }
+//              }
+//
+//          });
+//
+//    }
+//}
+//
+//
+//
+//
+
+
+
+
 package com.example.project_cal;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    EditText edt1, edt2, edt3;
+    Button btncong, btntru, btnnhan, btnchia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edt1 = findViewById(R.id.edta);
+        edt2 = findViewById(R.id.edtb);
+        edt3 = findViewById(R.id.edtc);
+        btncong = findViewById(R.id.btncong);
+        btntru = findViewById(R.id.btntru);
+        btnchia = findViewById(R.id.btnchia);
+        btnnhan = findViewById(R.id.btnnhan);
+
+        // Phép cộng
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    edt3.setText(String.format(getString(R.string.addition_result), a + b));
+                } catch (NumberFormatException e) {
+                    edt3.setText(getString(R.string.invalid_input_error));
+                }
+            }
+        });
+
+        // Phép trừ
+        btntru.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    edt3.setText(String.format(getString(R.string.subtraction_result), a - b));
+                } catch (NumberFormatException e) {
+                    edt3.setText(getString(R.string.invalid_input_error));
+                }
+            }
+        });
+
+        // Phép nhân
+        btnnhan.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    edt3.setText(String.format(getString(R.string.multiplication_result), a * b));
+                } catch (NumberFormatException e) {
+                    edt3.setText(getString(R.string.invalid_input_error));
+                }
+            }
+        });
+
+        // Phép chia
+        btnchia.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    if (b == 0) {
+                        edt3.setText(getString(R.string.division_by_zero_error));
+                    } else {
+                        edt3.setText(String.format(getString(R.string.division_result), a / b));
+                    }
+                } catch (NumberFormatException e) {
+                    edt3.setText(getString(R.string.invalid_input_error));
+                }
+            }
         });
     }
 }
