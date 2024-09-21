af8f0e0a9871176ebe11567fafdb8511874f1863
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-22 03:09:18 +0700

    Ex01

--- Demo_Constrain/app/src/main/java/com/example/demo_constrain/MainActivity.java
+++ Demo_Constrain/app/src/main/java/com/example/demo_constrain/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//
         setContentView(R.layout.activity_main);
         edtA = findViewById(R.id.edtA);
         edtB = findViewById(R.id.edtB);
