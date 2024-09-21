3064ad47e80f0947e01bedb409ddc2e98348cdec
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-21 09:01:46 +0700

    Giai_phuong_trinh_bac2

---
+++ Dang_ky_thong_tin_ca_nhan/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dang_ky_thong_tin_ca_nhan"
+include(":app")
+ 
