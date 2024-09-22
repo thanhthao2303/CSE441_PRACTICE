03cbe64941ba3a2b6cbb260d8d0ba7da03ac9e34
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-23 00:42:36 +0700

    Ex07_1

--- Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnopen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+         setContentView(R.layout.activity_main);
+//         btnopen = findViewById(R.id.btnopen);
+//         btnopen.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 Intent myintent = new Intent(MainA)
+//             }
+//         });
+
+        Button btn1 =(Button) findViewById(R.id.btnopen);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                Intent intent1 = new
+                        Intent(MainActivity.this,child_activity.class);
+                startActivity(intent1);
+            }
         });
+
+        };
     }
-}
+
+
+
+
