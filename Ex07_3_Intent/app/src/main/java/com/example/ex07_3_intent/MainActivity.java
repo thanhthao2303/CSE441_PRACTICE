7993865ec05aac077119ac73d94b0de5ac93c24a
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-24 01:05:04 +0700

    Ex07_3

--- Ex07_3_Intent/app/src/main/java/com/example/ex07_3_intent/MainActivity.java
+++ Ex07_3_Intent/app/src/main/java/com/example/ex07_3_intent/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.ex07_3_intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ EditText edta, edtb, edtkq;
+ Button btnrequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+         setContentView(R.layout.activity_main);
+         edta = findViewById(R.id.edta);
+         edtb = findViewById(R.id.edtb);
+         edtkq = findViewById(R.id.edtkq);
+         btnrequest = findViewById(R.id.btnrequest);
+         btnrequest.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent myintent = new
+                         Intent(MainActivity.this, activity_sub.class);
+//Lấy dữ liệu a, b
+                 int a =
+                         Integer.parseInt(edta.getText().toString());
+                 int b =
+                         Integer.parseInt(edtb.getText().toString());
+// Đẩy dữ liệu vào Intent
+                 myintent.putExtra("soa",a);
+                 myintent.putExtra("sob",b);
+
+                         startActivityForResult(myintent,99);
+             }
+         });
+             }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode,
+                resultCode, data);
+        if (requestCode == 99 && resultCode == 33)
+        {
+            assert data != null;
+            int sum = data.getIntExtra("kq",0);
+            edtkq.setText("Tổng 2 số là: "+sum);
+        }
+        if (requestCode == 99 && resultCode == 34)
+        {
+            assert data != null;
+            int sub = data.getIntExtra("kq",0);
+            edtkq.setText("Hiệu 2 số là: "+sub);
+        }
     }
-}
+    }
+
+
+
--- Ex07_3_Intent/app/src/main/java/com/example/ex07_3_intent/activity_sub.java
+++ Ex07_3_Intent/app/src/main/java/com/example/ex07_3_intent/activity_sub.java
@@ -1,24 +1,53 @@
 package com.example.ex07_3_intent;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class activity_sub extends AppCompatActivity {
-
+ EditText edtaa, edtbb;
+ Button btnsendtong, btnsendhieu;
+ Intent myintent;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sub);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+         setContentView(R.layout.activity_sub);
+         edtaa = findViewById(R.id.edtaa);
+         edtbb = findViewById(R.id.edtbb);
+       btnsendtong = findViewById(R.id.btnsendtong);
+       btnsendhieu = findViewById(R.id.btnsendhieu);
+       myintent = getIntent();
+       int a = myintent.getIntExtra("soa",0);
+       int b = myintent.getIntExtra("sob",0);
+       edtaa.setText(a+"");
+       edtbb.setText(b+"");
+       btnsendtong.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               int sum = a + b;
+                myintent.putExtra("kq",sum);
+                setResult(33,myintent);
+                finish();
+           }
+       });
+       btnsendhieu.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               int sub = a - b;
+               myintent.putExtra("kq",sub);
+               setResult(34, myintent);
+               finish();
+           }
+       });
     }
 }
+
+
+
+
