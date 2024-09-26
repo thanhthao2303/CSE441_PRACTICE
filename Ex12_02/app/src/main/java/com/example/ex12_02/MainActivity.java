d91b1498118b0fa18f8ec12a11e92a7477a632c2
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-26 11:54:16 +0700

    Ex12

--- Ex12_02/app/src/main/java/com/example/ex12_02/MainActivity.java
+++ Ex12_02/app/src/main/java/com/example/ex12_02/MainActivity.java
@@ -1,24 +1,86 @@
 package com.example.ex12_02;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+
+    ArrayList<String> arraywork;
+    ArrayAdapter<String> arrayAdapter;
+    EditText edtwork,edth,edtm;
+    TextView txtdate;
+    Button btnwork;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+         setContentView(R.layout.activity_main);
+            edth = findViewById(R.id.edthour);
+            edtm = findViewById(R.id.edtmi);
+            edtwork = findViewById(R.id.edtwork);
+            btnwork = findViewById(R.id.btnadd);
+            lv = findViewById(R.id.listView1);
+            txtdate = findViewById(R.id.txtdate);
+            arraywork = new ArrayList<>();
+            arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arraywork);
+            lv.setAdapter(arrayAdapter);
+            Date currentDate = Calendar.getInstance().getTime();
+        java.text.SimpleDateFormat simpleDateFormat = new
+                java.text.SimpleDateFormat("dd/MM/yyyy");
+        txtdate.setText("Hôm nay: "+simpleDateFormat.format(currentDate));
+        btnwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtwork.getText().toString().equals("")||edth.getText().toString().equals("")
+                        ||edtm.getText().toString().equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Info missing");
+                    builder.setMessage("Please enter all information of the work");
+                    builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    builder.show();
+
+                }
+                else {
+                    String str = edtwork.getText().toString() + " - " +
+                            edth.getText().toString() + ":" + edtm.getText().toString();
+
+
+                     arraywork.add(str);
+                     arrayAdapter.notifyDataSetChanged();
+                     edth.setText("");
+                    edtm.setText("");
+                    edtwork.setText("");
+                }
+            }
         });
     }
 }
+
+
+
--- ListVIew/app/src/main/java/com/example/listview/MainActivity.java
+++ ListVIew/app/src/main/java/com/example/listview/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.listview;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,20 +15,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView txt1;
+ListView lv1;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+         setContentView(R.layout.activity_main);
+         txt1 = findViewById(R.id.txt1);
+        final String[] arr1 = {"Iphone 7", "SamSung Galaxy S7",
+                "Nokia Lumia 730", "Sony Xperia XZ","HTC One E9"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr1);
+        ListView lv1 = findViewById(R.id.lv1);
+        lv1.setAdapter(adapter1);
+
+       lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> arg0, View arg1, int i, long arg3) {
+               txt1.setText("Vị trí "+i+ " : "+arr1[i]);
+
+           }
+       });
+
+     }
 }
+
+
+
+
 
 // deleloper.android.com
 // create dynamic lists with
 //gemini
+
+
