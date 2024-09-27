b842c7b51ece4a1f2497d5dbc782524a35da6cbb
thanhthao2303 <thao23032003ntt@gmail.com>
2024-09-27 22:12:08 +0700

    android

--- Ex13_1/app/src/main/java/com/example/ex13_1/MainActivity.java
+++ Ex13_1/app/src/main/java/com/example/ex13_1/MainActivity.java
@@ -8,8 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+ private List<country> countries;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        countries = new ArrayList<country>();
+        countries.add(new country(R.drawable.vn   ,"vietnam",countryCapital:""))
+        countries.add(new country(R.drawable.my  ,"my",))
+        countries.add(new country(R.drawable.ca  ,"canada",country))
+
+
     }
 }
