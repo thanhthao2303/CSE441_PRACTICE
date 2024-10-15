8d9117e0915aef67aef229626e49297d973a0dac
thanhthao2303 <thao23032003ntt@gmail.com>
2024-10-15 23:26:23 +0700

    android

--- Ex16/app/src/main/java/com/example/ex16/MainActivity.java
+++ Ex16/app/src/main/java/com/example/ex16/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex16;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,54 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edta, edtb, edtkq;
+    Button btntong, btnclear;
+    TextView txtlichsu;
+    String lichsu = "";
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edta = findViewById(R.id.edta);
+        edtb = findViewById(R.id.edtb);
+        edtkq = findViewById(R.id.edtkq);
+        btntong = findViewById(R.id.btntong);
+        btnclear = findViewById(R.id.btnclear);
+        txtlichsu = findViewById(R.id.txtlichsu);
+//------Lấy lại dữ liệu trong SharedPreferences--------
+        SharedPreferences myprefs =
+                getSharedPreferences("mysave",MODE_PRIVATE);
+        lichsu = myprefs.getString("ls","");
+        txtlichsu.setText(lichsu);
+//------------------------------------------
+        btntong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                int kq = a + b;
+                edtkq.setText(kq+"");
+                lichsu += a+" + "+b+" = "+kq;
+                txtlichsu.setText(lichsu);
+                lichsu +="\n"; // Xuống dòng
+            }
         });
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lichsu ="";
+                txtlichsu.setText(lichsu);
+            }
+        });
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences myprefs =
+                getSharedPreferences("mysave",MODE_PRIVATE);
+        SharedPreferences.Editor myedit = myprefs.edit();
+        myedit.putString("ls",lichsu);
+        myedit.commit();
     }
 }
