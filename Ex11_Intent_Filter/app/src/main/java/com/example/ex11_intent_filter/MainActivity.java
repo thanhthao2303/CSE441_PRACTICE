4b1564f4195bbe2510abc967a0ee4ca39e3bd03c
thanhthao2303 <thao23032003ntt@gmail.com>
2024-10-16 01:02:51 +0700

    android

---
+++ Ex11_Intent_Filter/app/src/main/java/com/example/ex11_intent_filter/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ex11_intent_filter;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        try {
+            WebView webView = findViewById(R.id.webview1);
+            webView.loadUrl(data.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
