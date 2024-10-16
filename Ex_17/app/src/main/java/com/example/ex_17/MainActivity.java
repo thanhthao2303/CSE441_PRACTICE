5cd6f2963592fb8b91f85beaf4a1f0002e408fc5
thanhthao2303 <thao23032003ntt@gmail.com>
2024-10-16 14:35:39 +0700

    android

--- Ex_17/app/src/main/java/com/example/ex_17/MainActivity.java
+++ Ex_17/app/src/main/java/com/example/ex_17/MainActivity.java
@@ -1,24 +1,99 @@
 package com.example.ex_17;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
-
+    String DB_PATH_SUFFIX = "/databases/";
+    SQLiteDatabase database=null;
+    String DATABASE_NAME="quanlisach.db";
+    //Khai báo ListView
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//Gọi hàm Copy CSDL từ assets vào thư mục Databases
+        processCopy();
+//Mở CSDL lên để dùng
+        database = openOrCreateDatabase("quanlisach.db",MODE_PRIVATE, null);
+// Tạo ListView
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+// Truy vấn CSDL và cập nhật hiển thị lên Listview
+        Cursor c = database.query("tbsach",null,null,null,null,null,null);
+        c.moveToFirst();
+        String data ="";
+        while (c.isAfterLast() == false)
+        {
+            data = c.getString(0)+"-"+c.getString(1)+"-"+c.getString(2);
+            mylist.add(data);
+            c.moveToNext();
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
+    }
+    private void processCopy() {
+//private app
+        File dbFile = getDatabasePath(DATABASE_NAME);
+        if (!dbFile.exists())
+        {
+            try{CopyDataBaseFromAsset();
+                Toast.makeText(this, "Copying sucess from Assets folder",
+                        Toast.LENGTH_LONG).show();
+            }
+            catch (Exception e){
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    private String getDatabasePath() {
+        return getApplicationInfo().dataDir + DB_PATH_SUFFIX+ DATABASE_NAME;
+    }
+    public void CopyDataBaseFromAsset() {
+// TODO Auto-generated method stub
+        try {
+            InputStream myInput;
+            myInput = getAssets().open(DATABASE_NAME);
+// Path to the just created empty db
+            String outFileName = getDatabasePath();
+// if the path doesn't exist first, create it
+            File f = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+            if (!f.exists())
+                f.mkdir();
+// Open the empty db as the output stream
+            OutputStream myOutput = new FileOutputStream(outFileName);
+// transfer bytes from the inputfile to the outputfile
+// Truyền bytes dữ liệu từ input đến output
+            int size = myInput.available();
+            byte[] buffer = new byte[size];
+            myInput.read(buffer);
+            myOutput.write(buffer);
+// Close the streams
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+        } catch (IOException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
--- Ex_19/app/src/main/java/com/example/ex_19/MainActivity.java
+++ Ex_19/app/src/main/java/com/example/ex_19/MainActivity.java
@@ -1,24 +1,84 @@
 package com.example.ex_19;
 
 import android.os.Bundle;
+import android.util.Xml;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ex_19.R;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnparse;
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+         setContentView(R.layout.activity_main);
+
+        btnparse = findViewById(R.id.btnparse);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+
+        btnparse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parsexml();
+            }
         });
+    }
+
+    private void parsexml() {
+        try {
+            InputStream myinput = getAssets().open("employee.xml");
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setInput(myinput, null);
+
+            int eventType = parser.getEventType();
+            String nodeName;
+            String datashow = "";
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        nodeName = parser.getName();
+                        if (nodeName.equals("employee")) {
+                            datashow = parser.getAttributeValue(null, "id") + "-" + parser.getAttributeValue(null, "title") + "-";
+                        } else if (nodeName.equals("name")) {
+                            datashow += parser.nextText() + "-";
+                        } else if (nodeName.equals("phone")) {
+                            datashow += parser.nextText();
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        nodeName = parser.getName();
+                        if (nodeName.equals("employee")) {
+                            mylist.add(datashow);
+                            datashow = "";
+                        }
+                        break;
+                }
+                eventType = parser.next();
+            }
+            myadapter.notifyDataSetChanged();
+        } catch (XmlPullParserException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
