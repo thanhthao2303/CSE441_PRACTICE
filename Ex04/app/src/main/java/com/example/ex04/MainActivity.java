72352978c201d8180e474fdebe32016d1b01ba06
thanhthao2303 <thao23032003ntt@gmail.com>
2024-10-20 23:29:20 +0700

    Ex04

---
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.ex04;
+
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends Activity {
+    EditText txtCel,txtFar;
+    Button btnCel, btnFar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+        txtFar = findViewById(R.id.txtFar);
+        txtCel = findViewById(R.id.txtCel);
+        btnFar = findViewById(R.id.btnFar);
+        btnCel = findViewById(R.id.btnCel);
+        btnCel.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcf=new DecimalFormat("#.00");
+                String doC = txtCel.getText()+"";
+                int C=Integer.parseInt(doC);
+                txtFar.setText(dcf.format(C * 1.8 + 32));
+            }
+        });
+        btnFar.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcf=new DecimalFormat("#.00");
+// TODO Auto-generated method stub
+                String doF = txtFar.getText()+"";
+                int F=Integer.parseInt(doF);
+                txtCel.setText(dcf.format((F - 32) / 1.8));
+            }
+        });
+
+
+    }
+}
+
+
+
